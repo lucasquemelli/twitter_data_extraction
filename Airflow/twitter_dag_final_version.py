d305a0df5ca5f9463cf734a1ea3820ede976ec4f
lucasquemelli <lucasquemelli@gmail.com>
2021-12-15 15:38:34 -0300

    Latest commit

--- Airflow/twitter_dag_final_version.py
+++ Airflow/twitter_dag_final_version.py
@@ -4,6 +4,7 @@ sys.path.append("[/home/lucasquemelli/datapipeline/airflow/plugins]")
 from datetime import datetime
 from os.path import join
 from pathlib import Path
+
 from airflow.models import DAG
 from airflow.operators.alura import TwitterOperator
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
@@ -12,15 +13,12 @@ from airflow.utils.dates import days_ago
 ARGS = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": days_ago(6)
+    "start_date": days_ago(6),
 }
 
-BASE_FOLDER = join(
-    str(Path("~/datapipeline").expanduser()),
-    "datalake/{stage}/twitter_aluraonline/{partition}"
-)
+#BASE_FOLDER = "/home/lucasquemelli/datapipeline/datalake/{stage}/twitter_aluraonline/{partition}"
 
-PARTITION_FOLDER = "extract_date={{ ds }}"
+#PARTITION_FOLDER = "extract_date={{ ds }}"
 
 TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
 
@@ -29,38 +27,32 @@ with DAG(dag_id="twitter_dag",
          schedule_interval="0 9 * * *",
          max_active_runs=1) as dag:
     twitter_operator = TwitterOperator(
-        task_id = "twitter_aluraonline",
-        query = "AluraOnline",
-        file_path =join(
-            BASE_FOLDER.format(stage="bronze", partition=PARTITION_FOLDER),
-            "AluraOnline_{{ ds_nodash }}.json"
-        ),
+        task_id="twitter_aluraonline",
+        query="AluraOnline",
+        file_path="/home/lucasquemelli/datapipeline/datalake/bronze/twitter_aluraonline/extract_date={{ ds }}",
         start_time=(
             "{{"
-            f"execution_date.stftime('{TIMESTAMP_FORMAT}')"
+            f"execution_date.strftime('{TIMESTAMP_FORMAT}')"
             "}}"
         ),
         end_time=(
             "{{"
-            f"next_execution_date.stftime('{TIMESTAMP_FORMAT}')"
+            f"next_execution_date.strftime('{TIMESTAMP_FORMAT}')"
             "}}"
         )
     )
 
     twitter_transform = SparkSubmitOperator(
         task_id = "transformation_twitter_aluraonline",
-        application = join(
-            str(Path(__file__).parents[2]),
-            "spark/transformation_final_version.py"
-        ),
+        application = "/home/lucasquemelli/datapipeline/spark/transformation_final_version.py",
         name = "twitter_transformation",
         application_args = [
             "--src",
-            BASE_FOLDER.format(stage="bronze", partition=PARTITION_FOLDER),
+            "/home/lucasquemelli/datapipeline/datalake/bronze/twitter_aluraonline/extract_date={{ ds }}",
             "--dest",
-            BASE_FOLDER.format(stage="silver", partition=""),
+            "/home/lucasquemelli/datapipeline/datalake/silver/twitter_aluraonline",
             "--process_date",
-            "{{ ds }}"
+            "{{ ds }}",
         ]
     )
 
