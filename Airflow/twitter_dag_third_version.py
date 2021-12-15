cf1279712fc4cabc905f7104f7909e5139bba486
lucasquemelli <lucasquemelli@gmail.com>
2021-12-14 22:19:08 -0300

    Latest commit

--- Airflow/twitter_dag_third_version.py
+++ Airflow/twitter_dag_third_version.py
@@ -3,7 +3,6 @@ sys.path.append("[/home/lucasquemelli/datapipeline/airflow/plugins]")
 
 from datetime import datetime
 from os.path import join
-from pathlib import Path
 from airflow.models import DAG
 from airflow.operators.alura import TwitterOperator
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
@@ -15,13 +14,6 @@ ARGS = {
     "start_date": days_ago(6)
 }
 
-BASE_FOLDER = join(
-    str(Path("~/datapipeline").expanduser()),
-    "datalake/{stage}/twitter_aluraonline/{partition}"
-)
-
-PARTITION_FOLDER = "extract_date={{ ds }}"
-
 TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
 
 with DAG(dag_id="twitter_dag",
@@ -32,7 +24,9 @@ with DAG(dag_id="twitter_dag",
         task_id = "twitter_aluraonline",
         query = "AluraOnline",
         file_path =join(
-            BASE_FOLDER.format(stage="bronze", partition=PARTITION_FOLDER),
+            "/home/lucasquemelli/datapipeline/datalake",
+            "twitter_aluraonline",
+            "extract_date={{ ds }}",
             "AluraOnline_{{ ds_nodash }}.json"
         ),
         start_time=(
@@ -49,16 +43,13 @@ with DAG(dag_id="twitter_dag",
 
     twitter_transform = SparkSubmitOperator(
         task_id = "transformation_twitter_aluraonline",
-        application = join(
-            str(Path(__file__).parents[2]),
-            "spark/transformation_final_version.py"
-        ),
+        application = "/home/lucasquemelli/datapipeline/spark/transformation_final_version.py",
         name = "twitter_transformation",
         application_args = [
             "--src",
-            BASE_FOLDER.format(stage="bronze", partition=PARTITION_FOLDER),
+            "/home/lucasquemelli/datapipeline/datalake/bronze/twitter_aluraonline/extract_date=2021-12-05",
             "--dest",
-            BASE_FOLDER.format(stage="silver", partition=""),
+            "/home/lucasquemelli/datapipeline/datalake/silver/twitter_aluraonline",
             "--process_date",
             "{{ ds }}"
         ]
